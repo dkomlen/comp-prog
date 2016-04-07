631ad08f3f8f1ec76a5da4d3b64f1ed835832c12
Danko Komlen <dkomlen@gmail.com>
2016-04-07 01:50:36 +0100

    Replace tabs with spaces

--- codejam/minimum-scalar-product.cpp
+++ codejam/minimum-scalar-product.cpp
@@ -6,26 +6,26 @@ using namespace std;
 #define ll long long int
 
 void read(vector<ll> &v, ll n) {
-	for (int j = 0; j < n; ++j) {
-		int x; cin >> x; v[j] = x;
-	}
+   for (int j = 0; j < n; ++j) {
+      int x; cin >> x; v[j] = x;
+   }
 }
 
 int main() {
-	int t; cin >> t;
-	for (int i = 0; i < t; ++i) {
-		ll n, sol = 0; cin >> n;
-		vector<ll> v(n),w(n);
-		read(v,n);
-		read(w,n);
+   int t; cin >> t;
+   for (int i = 0; i < t; ++i) {
+      ll n, sol = 0; cin >> n;
+      vector<ll> v(n),w(n);
+      read(v,n);
+      read(w,n);
 
-		sort(v.begin(), v.end());
-		sort(w.rbegin(), w.rend());
+      sort(v.begin(), v.end());
+      sort(w.rbegin(), w.rend());
 
-		for (int j = 0; j < n; j++) {
-			sol += v[j] * w[j];
-		}
-		cout << "Case #" << i + 1 << ": " << sol << endl;
-	}
-	return 0;
+      for (int j = 0; j < n; j++) {
+         sol += v[j] * w[j];
+      }
+      cout << "Case #" << i + 1 << ": " << sol << endl;
+   }
+   return 0;
 }
--- codejam/reverse-words.cpp
+++ codejam/reverse-words.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int main() {
-	int n; cin >> n;
-	for(int i = 0; i < n; ++i) {
-		string x,y;
-		while (cin >> x) {
-			y = x + " " + y;
-			if (cin.peek() == '\n') break;
-		}
-		cout << "Case #" << i + 1 << ": " << y << endl;
-	}
-	return 0;
+   int n; cin >> n;
+   for(int i = 0; i < n; ++i) {
+      string x,y;
+      while (cin >> x) {
+         y = x + " " + y;
+         if (cin.peek() == '\n') break;
+      }
+      cout << "Case #" << i + 1 << ": " << y << endl;
+   }
+   return 0;
 }
