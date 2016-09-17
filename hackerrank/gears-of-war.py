1222708e8c6257e7d2ad01d4fb3de1cc68b55ce1
Danko Komlen <dkomlen@gmail.com>
2016-09-17 20:41:47 +0100

    Solved gears of war

---
+++ hackerrank/gears-of-war.py
@@ -0,0 +1,5 @@
+#  Week of Code 23, Gears of War
+# https://www.hackerrank.com/contests/w23/challenges/gears-of-war
+
+for i in xrange(int(raw_input())):
+   print("No" if (int(raw_input()) % 2) else "Yes")
