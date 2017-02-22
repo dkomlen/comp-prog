c0efbe0d2ed4a201e8207eaec435ac23859fb2e5
Danko Komlen <dkomlen@gmail.com>
2017-02-22 00:20:57 +0000

    Removing extra files added

--- hackerrank/regex/find-hackerrank.py
+++
@@ -1,7 +0,0 @@
-import re
-n, hr = None, 'hackerrank'
-for i in range(int(raw_input())):
-	p = re.compile(r'^(%s)?.*?(%s)?$' % (hr, hr))
-	s = raw_input()
-	m = p.match(s).groups() if s != hr else (hr,hr)	
-	print {(n,n):-1, (hr,n):1, (n,hr):2, (hr,hr):0}[m]
--- hackerrank/regex/say-hi.py
+++
@@ -1,6 +0,0 @@
-import re
-n, hr = None, 'hackerrank'
-for i in range(int(raw_input())):
-	p = re.compile(r'hi\s[^d].*', re.IGNORECASE)
-	s = raw_input()
-	if p.match(s): print s
